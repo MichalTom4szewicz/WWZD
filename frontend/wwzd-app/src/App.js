abef324538d42d2b5f25570aa06b558bd68e502f
Marek Sołdaczuk <forexampleee@wp.pl>
2021-10-10 21:24:17 +0200

    (frontend) wstepna konfiguracja plotly.js

---
+++ frontend/wwzd-app/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import { RandomChart } from "./RandomChart";
+
+export const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <RandomChart />
+      </header>
+    </div>
+  );
+};
