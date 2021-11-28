58f3c83b768673c2339bdf60b79c4ca3fbfc6fe2
MichalTom4szewicz <235568@student.pwr.edu.pl>
2021-11-29 00:48:43 +0100

    Added PCA analysis *sending at least 4 files will trigger analysis and will return to 	frontend x-y-z coordinates of 4 uploaded photos
    Signed-off-by: MichalTom4szewicz <235568@student.pwr.edu.pl>

--- backend/main.py
+++ backend/main.py
@@ -10,7 +10,9 @@ from PIL import Image
 from numpy import asarray
 import numpy as np
 import math
+from multiprocessing import Value
 
+counter = Value('i', 0)
 
 app = Flask(__name__)
 CORS(app)
@@ -50,7 +52,7 @@ def to_gray(img):
 
     # gray_img
     np_gray = np.array(gray_img)
-    return np_gray.reshape((w, h))
+    return np_gray  # .reshape((w, h))
 
 
 def file_coordinates():
@@ -70,6 +72,8 @@ def hello_world():
 
 @app.route('/file', methods=['POST'])
 def handle_file():
+
+    numba = 4
     # data = request.get_json()
 
     print(request.files)
@@ -77,22 +81,91 @@ def handle_file():
 
     # f.save(secure_filename(f.filename))
     ext = f.filename.split(".")[1]
-    f.save(secure_filename(f"img.{ext}"))
+    with counter.get_lock():
+        # counter.value += 1
 
-    image = Image.open(f"img.{ext}")
+        f.save(secure_filename(f"img{counter.value}.{ext}"))
+        counter.value += 1
+        val = counter.value
 
-    image_data = asarray(image)
+    if val > numba-1:
 
-    gray = to_gray(image_data)
+        ims = []
+        for i in range(numba):
+            image = Image.open(f"img{i}.{ext}")
+            image_data = asarray(image)
+            image_data = to_gray(image_data)
+            ims.append(image_data)
 
-    pca = PCA(.95)
-    pca.fit(gray)
+        imgs = np.array(ims)
+        print(imgs.shape)
 
-    print(pca.explained_variance_ratio_)
-    print(pca.singular_values_)
+        pca = PCA(3)
+        p_comp = pca.fit_transform(imgs)
+        print(p_comp.shape, p_comp)
 
-    return file_coordinates()
+        n = []
+        for pair3 in p_comp:
+            x, y, z = pair3
+            if(x < 0):
+                x = math.log10(abs(int(x)))
+                x *= -1
+            else:
+                x = math.log10(abs(int(x)))
+
+            if(y < 0):
+                y = math.log10(abs(int(y)))
+                y *= -1
+            else:
+                y = math.log10(abs(int(y)))
+
+            if(z < 0):
+                z = math.log10(abs(int(z)))
+                z *= -1
+            else:
+                z = math.log10(abs(int(z)))
+
+            pair = x, y, z
+            n.append(pair)
+
+        print(n)
+
+        obs = []
+        for i in range(numba):
+            x, y, z = n[i]
+            tmp = {
+                "x": x,
+                "y": y,
+                "z": z
+            }
+            obs.append(tmp)
+
+        value = {
+            "status": "ready",
+            "data": obs
+        }
+
+        return json.dumps(value)
+
+    # image = Image.open(f"img.{ext}")
+
+    # image_data = asarray(image)
+
+    # gray = to_gray(image_data)
+    # gray = np.array([gray])
+
+    # print(gray.shape)
+
+    # pca = PCA(0.8)
+    # # gray = gray.reshape(1, -1)
+    # p_comp = pca.fit_transform(gray)
+
+    # print(p_comp.shape)
+
+    # return file_coordinates()
+    return json.dumps({"status": "not-ready"})
 
 
 if __name__ == '__main__':
+
     app.run(debug=True, port=5000)
--- frontend/wwzd-app/src/MyUploader.js
+++ frontend/wwzd-app/src/MyUploader.js
@@ -6,12 +6,21 @@ const MyUploader = () => {
     const getUploadParams = ({ meta }) => { return { url: 'http://127.0.0.1:5000/file' } }
 
     // called every time a file's `status` changes
-    const handleChangeStatus = ({ meta, file }, status) => { console.log(status, meta, file) }
+    const handleChangeStatus = ({ meta, file, xhr }, status) => {
+
+        /*console.log(status, meta, file)*/
+        if (status === 'done'){
+            let response = JSON.parse(xhr.response);
+
+            console.log(response)
+        }
+    }
 
     // receives array of files that are done uploading when submit button is clicked
     const handleSubmit = (files, allFiles) => {
-      console.log(files.map(f => f.meta))
+    //   console.log(files.map(f => f.meta))
       allFiles.forEach(f => f.remove())
+
     }
 
     return (
