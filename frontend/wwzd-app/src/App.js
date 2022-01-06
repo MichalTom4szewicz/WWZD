6e4c4e94579e0735c3af1b0a67605e9d92a93a7b
Marek Sołdaczuk <forexampleee@wp.pl>
2022-01-06 18:37:07 +0100

    (frontend) setup pod grupowanie klas + wyświetlanie zdjec

--- frontend/wwzd-app/src/App.js
+++ frontend/wwzd-app/src/App.js
@@ -5,19 +5,19 @@ import { MainChart } from "./components/MainChart";
 import fileService from "./services/file";
 
 const options = [
-  { value: 'pickup', label: 'pickup' },
-  { value: 'convertible', label: 'convertible' },
-  { value: 'sports_car', label: 'sports_car' },
-  { value: 'minivan', label: 'minivan' },
-  { value: 'beach_wagon', label: 'beach_wagon' },
-  { value: 'limousine', label: 'limousine' },
-  { value: 'racer', label: 'racer' },
-  { value: 'jeep', label: 'jeep' },
-  { value: 'tow_truck', label: 'tow_truck' },
-  { value: 'cab', label: 'cab' },
-  { value: 'minibus', label: 'minibus' },
-  { value: 'passenger_car', label: 'passenger_car' },
-]
+  { value: "pickup", label: "pickup" },
+  { value: "convertible", label: "convertible" },
+  { value: "sports_car", label: "sports_car" },
+  { value: "minivan", label: "minivan" },
+  { value: "beach_wagon", label: "beach_wagon" },
+  { value: "limousine", label: "limousine" },
+  { value: "racer", label: "racer" },
+  { value: "jeep", label: "jeep" },
+  { value: "tow_truck", label: "tow_truck" },
+  { value: "cab", label: "cab" },
+  { value: "minibus", label: "minibus" },
+  { value: "passenger_car", label: "passenger_car" },
+];
 
 const Checkbox = ({ label, value, onChange }) => {
   return (
@@ -30,7 +30,8 @@ const Checkbox = ({ label, value, onChange }) => {
 
 const Select = ({ label, value, onChange }) => {
   return (
-    <label>{label}:
+    <label>
+      {label}:
       <select value={value} onChange={onChange}>
         {options.map((option) => (
           <option value={option.value}>{option.label}</option>
@@ -45,46 +46,55 @@ export const App = () => {
   const [yArray, setYArray] = useState([]);
   const [zArray, setZArray] = useState([]);
   const [namesArray, setNamesArray] = useState([]);
+  const [classesArray, setClassesArray] = useState([]);
 
   const [method, setMethod] = useState([1, 0, 0]);
   const [ifClass, setIfClass] = useState(false);
   const [axes, setAxes] = useState(["pickup", "convertible", "sports_car"]);
-  const methodsArray = ["pca", "umap", "no_whiten", "pca_no_norm", "umap_no_norm"]
+  const methodsArray = [
+    "pca",
+    "umap",
+    "no_whiten",
+    "pca_no_norm",
+    "umap_no_norm",
+  ];
 
   const handleMethod = (index) => {
-    let met = method.map(m => 0)//Math.abs(m-1))
-    met[index] = 1
-    setMethod(met)
-    fileService.changeMethod({"method": methodsArray[index]}).then((response) => {
-      console.log(response)
-      // let response = JSON.parse(xhr.response);
+    let met = method.map((m) => 0); //Math.abs(m-1))
+    met[index] = 1;
+    setMethod(met);
+    fileService
+      .changeMethod({ method: methodsArray[index] })
+      .then((response) => {
+        console.log(response);
+        // let response = JSON.parse(xhr.response);
 
-      const cords = response.data;
-      setXArray(cords.map((item) => item.x));
-      setYArray(cords.map((item) => item.y));
-      setZArray(cords.map((item) => item.z));
-      setNamesArray(cords.map((item) => item.filename));
-    })
-  }
+        const cords = response.data;
+        setXArray(cords.map((item) => item.x));
+        setYArray(cords.map((item) => item.y));
+        setZArray(cords.map((item) => item.z));
+        setNamesArray(cords.map((item) => item.filename));
+        // setClassesArray(cords.map((item) => item.className));
+      });
+  };
 
   const handleAxes = (e, i) => {
     let ax = [...axes];
     ax[i] = e.target.value;
     setAxes(ax);
     if (ifClass) {
-      fileService.changeAxes({"axes": ax}).then((response) => {
-        console.log(response)
-  
+      fileService.changeAxes({ axes: ax }).then((response) => {
+        console.log(response);
+
         const cords = response.data;
         setXArray(cords.map((item) => item.x));
         setYArray(cords.map((item) => item.y));
         setZArray(cords.map((item) => item.z));
         setNamesArray(cords.map((item) => item.filename));
-      })
+        // setClassesArray(cords.map((item) => item.className));
+      });
     }
-  }
-
-  // const [image, setImage] = useState("");
+  };
 
   // const handleClick = (e) => {
   //   e.preventDefault();
@@ -101,7 +111,6 @@ export const App = () => {
     <div className="App">
       <div id="left">
         <header className="App-header">
-
           <MainChart
             xArray={xArray}
             setXArray={setXArray}
@@ -112,6 +121,8 @@ export const App = () => {
             namesArray={namesArray}
             setNamesArray={setNamesArray}
             axesNames={ifClass ? axes : undefined}
+            classesArray={classesArray}
+            setClassesArray={setClassesArray}
           />
         </header>
       </div>
@@ -121,48 +132,48 @@ export const App = () => {
 
         <Checkbox
           label="PCA"
-          value = {method[0] ? true : false}
-          onChange={()=>handleMethod(0)}
+          value={method[0] ? true : false}
+          onChange={() => handleMethod(0)}
         />
         <Checkbox
           label="UMAP"
-          value = {method[1] ? true : false}
-          onChange={()=>handleMethod(1)}
+          value={method[1] ? true : false}
+          onChange={() => handleMethod(1)}
         />
         <Checkbox
           label="PCA (no whitening)"
-          value = {method[2] ? true : false}
-          onChange={()=>handleMethod(2)}
+          value={method[2] ? true : false}
+          onChange={() => handleMethod(2)}
         />
         <Checkbox
           label="PCA (no normalization)"
-          value = {method[3] ? true : false}
-          onChange={()=>handleMethod(3)}
+          value={method[3] ? true : false}
+          onChange={() => handleMethod(3)}
         />
         <Checkbox
           label="UMAP (no normalization)"
-          value = {method[4] ? true : false}
-          onChange={()=>handleMethod(4)}
+          value={method[4] ? true : false}
+          onChange={() => handleMethod(4)}
         />
         <Checkbox
           label="Class"
-          value = {ifClass}
-          onChange={()=>setIfClass(!ifClass)}
+          value={ifClass}
+          onChange={() => setIfClass(!ifClass)}
         />
         <p>
           <Select
             label="X"
-            value = {axes[0]}
+            value={axes[0]}
             onChange={(e) => handleAxes(e, 0)}
           />
           <Select
             label="Y"
-            value = {axes[1]}
+            value={axes[1]}
             onChange={(e) => handleAxes(e, 1)}
           />
           <Select
             label="Z"
-            value = {axes[2]}
+            value={axes[2]}
             onChange={(e) => handleAxes(e, 2)}
           />
         </p>
--- frontend/wwzd-app/src/components/MainChart.tsx
+++ frontend/wwzd-app/src/components/MainChart.tsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Plot from "react-plotly.js";
+import "../styles/App.css";
 
-// export type Cord = [x: number, y: number, z: number, objectClass: string];
 export type Cord = {
   x: number;
   y: number;
@@ -21,11 +21,18 @@ export const MainChart = ({
   namesArray,
   setNamesArray,
   axesNames,
+  classesArray,
+  setClassesArray,
 }) => {
   const [colors, setColors] = useState<string[]>([]);
   const [loading, setLoading] = useState(true);
-  // const [classesArray, setClassesArray] = useState<string[]>([]);
   // const [initLength, setInitLength] = useState(0);
+  const [image, setImage] = useState("");
+  const [camera, setCamera] = useState({
+    up: { x: 0, y: 0, z: 1 },
+    center: { x: 0, y: 0, z: 0 },
+    eye: { x: 1.25, y: 1.25, z: 1.25 },
+  });
 
   useEffect(() => {
     const getImgs = async () => {
@@ -36,15 +43,69 @@ export const MainChart = ({
         const colorsArray = [];
         setXArray(
           cords.map((item) => {
-            colorsArray.push("red");
+            switch (item.className) {
+              case "pickup": {
+                colorsArray.push("blue");
+                break;
+              }
+              case "convertible": {
+                colorsArray.push("orange");
+                break;
+              }
+              case "minivan": {
+                colorsArray.push("green");
+                break;
+              }
+              case "beach_wagon": {
+                colorsArray.push("yellow");
+                break;
+              }
+              case "sports_car": {
+                colorsArray.push("red");
+                break;
+              }
+              case "limousine": {
+                colorsArray.push("white");
+                break;
+              }
+              case "racer": {
+                colorsArray.push("pink");
+                break;
+              }
+              case "jeep": {
+                colorsArray.push("silver");
+                break;
+              }
+              case "cab": {
+                colorsArray.push("brown");
+                break;
+              }
+              case "minibus": {
+                colorsArray.push("purple");
+                break;
+              }
+              case "passenger_car": {
+                colorsArray.push("magenta");
+                break;
+              }
+              case "other": {
+                colorsArray.push("black");
+                break;
+              }
+              default: {
+                colorsArray.push("grey");
+                break;
+              }
+            }
+
             return item.x;
           })
         );
         setYArray(cords.map((item) => item.y));
         setZArray(cords.map((item) => item.z));
         setNamesArray(cords.map((item) => item.filename));
+        // setClassesArray(cords.map((item) => item.className));
         setColors(colorsArray);
-        // setClassesArray(cords.map((item) => item[3]));
       } catch (e) {
         console.log({ e });
       }
@@ -58,36 +119,58 @@ export const MainChart = ({
       {loading ? (
         <>Ładowanie...</>
       ) : (
-        <Plot
-          data={[
-            {
-              x: xArray,
-              y: yArray,
-              z: zArray,
-              text: namesArray,
-              // name: "name",
-              mode: "markers",
-              type: "scatter3d",
-              marker: {
-                color: colors,
-                size: 5,
-                // symbol: ["diamond-open"],
+        <>
+          <img className="image" src={image} alt="..." />
+
+          <Plot
+            data={[
+              {
+                x: xArray,
+                y: yArray,
+                z: zArray,
+                text: namesArray,
+                name: classesArray || "name",
+                mode: "markers",
+                type: "scatter3d",
+                marker: {
+                  color: colors,
+                  size: 5,
+                  // symbol: ["diamond-open"],
+                },
+                hoverinfo: "all",
               },
-              hoverinfo: "x+y+z+text",
-            },
-          ]}
-          layout={{
-            scene: {
-              xaxis: { title: axesNames ? axesNames[0] : "oś X" },
-              yaxis: { title: axesNames ? axesNames[1] : "oś Y" },
-              zaxis: { title: axesNames ? axesNames[2] : "oś Z" },
-            },
-            height: 800,
-            width: 800,
-            title: "Wizualizacja wielkich zbiorów danych",
-          }}
-          // onRelayout={(figure) => console.log(figure)}
-        />
+            ]}
+            layout={{
+              scene: {
+                xaxis: { title: axesNames ? axesNames[0] : "oś X" },
+                yaxis: { title: axesNames ? axesNames[1] : "oś Y" },
+                zaxis: { title: axesNames ? axesNames[2] : "oś Z" },
+                camera: camera,
+              },
+              height: 800,
+              width: 800,
+              title: "Wizualizacja wielkich zbiorów danych",
+            }}
+            revision={0}
+            onHover={(e: any) => {
+              setImage(e.points[0]?.text || "");
+            }}
+            onRelayout={(figure: any) => {
+              // console.log(figure);
+              if (
+                figure["scene.camera"]?.center &&
+                figure["scene.camera"]?.up &&
+                figure["scene.camera"]?.eye
+              ) {
+                setCamera({
+                  center: figure["scene.camera"].center,
+                  up: figure["scene.camera"].up,
+                  eye: figure["scene.camera"].eye,
+                });
+              }
+            }}
+          />
+        </>
       )}
     </>
   );
